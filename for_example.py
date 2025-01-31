a27fc185b5662dae2c9af486f4ec1d3ffcf6b6a3
Oscar <olin.ieu2023@student.ie.edu>
2025-01-31 11:12:20 +0100

    For example

---
+++ for_example.py
@@ -0,0 +1,16 @@
+for i in range (1, 10, 2):
+    print(i)
+
+
+s = 0
+for i in range(1, 11):
+    s += i
+    print(s)
+
+i = 1
+s = 0
+while i < 11:
+    s += i
+    i += 1
+
+print("sum using while is also", s) 
